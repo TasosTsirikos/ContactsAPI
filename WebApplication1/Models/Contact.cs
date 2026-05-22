c4d376fa9d6dc1bb3cd65a93b0ad1c33ad9fadd0
Anastasios Tsirikos <tsirikos.anastasios@nbg.gr>
2026-05-22 11:30:31 +0300

    Create ORM Entity Framework and connect with DB and Model Contact

---
+++ WebApplication1/Models/Contact.cs
@@ -0,0 +1,13 @@
+namespace WebApplication1.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName {  get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
